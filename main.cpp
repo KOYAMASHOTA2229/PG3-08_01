fa620f23dee609b466f4fbac3dcc36bf2723eee3
KoyamaShota <k023g1039@g.neec.ac.jp>
2024-08-02 16:30:41 +0900

    08_01

---
+++ main.cpp
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <iostream>
+#include <chrono>
+
+int main() {
+
+	std::string a(1000000, 'a');
+
+	std::chrono::system_clock::time_point start, end;
+	std::time_t time_stamp;
+
+	//計測開始
+	start =  std::chrono::system_clock::now();
+
+	//計測したい処理
+	//コピー
+	std::string b = a;
+
+	//計測終了
+	end = std::chrono::system_clock::now();
+
+
+	// 処理に要した時間
+	auto time1 = end - start;
+
+	// 処理に要した時間をミリ秒に変換
+	auto msec1 = std::chrono::duration_cast<std::chrono::microseconds>(time1).count();
+	std::cout << msec1 << " msec" << std::endl;
+
+
+
+	//計測開始
+	start = std::chrono::system_clock::now();
+
+	//計測したい処理
+	//移動
+	std::string m = move(a);
+
+	//計測終了
+	end = std::chrono::system_clock::now();
+
+
+	// 処理に要した時間
+	auto time2 = end - start;
+
+	// 処理に要した時間をミリ秒に変換
+	auto msec2 = std::chrono::duration_cast<std::chrono::microseconds>(time2).count();
+	std::cout << msec2 << " msec" << std::endl;
+
+
+	return 0;
+}
